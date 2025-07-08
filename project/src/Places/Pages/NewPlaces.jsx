0e5c3c58eeae08210fdccf307dbdff3c345606a7
Tayyba-Zulfiqar <tayybazulfiqar786@gmail.com>
2025-07-09 01:15:22 +0500

    added basic validation on entering new places data

--- project/src/Places/Pages/NewPlaces.jsx
+++ project/src/Places/Pages/NewPlaces.jsx
@@ -1,3 +1,16 @@
+import "./NewPlaces.css";
+import Input from "../../Shared/Components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../Shared/Util/Validators";
 export default function NewPlaces() {
-  return <h1>New places</h1>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="please enter a valid title"
+      />
+    </form>
+  );
 }
